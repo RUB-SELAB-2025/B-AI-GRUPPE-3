913d29a2960849c74fe928c2dad81b65550a7c11
Ataneko161 <info@tentacle.nexus>
2025-06-18 14:39:36 +0200

    Added green box for g group and started implementing https://codepen.io/dabrorius/pen/EdQoYe

--- angular-frontend/src/app/graph/graph.component.ts
+++ angular-frontend/src/app/graph/graph.component.ts
@@ -11,7 +11,7 @@ import {
 } from '@angular/core';
 import { transition } from 'd3';
 import { axisBottom, axisLeft } from 'd3-axis';
-import { select } from 'd3-selection';
+import { select, selectAll } from 'd3-selection';
 import { DeviceListComponent } from "../omnai-datasource/omnai-scope-server/devicelist.component";
 import { ResizeObserverDirective } from '../shared/resize-observer.directive';
 import { StartDataButtonComponent } from "../source-selection/start-data-from-source.component";
@@ -31,6 +31,7 @@ export class GraphComponent {
   readonly svgGraph = viewChild.required<ElementRef<SVGElement>>('graphContainer');
   readonly axesContainer = viewChild.required<ElementRef<SVGGElement>>('xAxis');
   readonly axesYContainer = viewChild.required<ElementRef<SVGGElement>>('yAxis');
+  readonly scrollContainer = viewChild.required<ElementRef<SVGGElement>>('scrollgroup');
 
   private readonly platform = inject(PLATFORM_ID);
   isInBrowser = isPlatformBrowser(this.platform);
@@ -53,16 +54,18 @@ export class GraphComponent {
   // Axes related computations and
 
   marginTransform = computed(() => {
-    return `translate(${this.dataservice.margin.left}, ${this.dataservice.margin.top})`
+    return `translate(${this.dataservice.margin.left}, 0)` //${this.dataservice.margin.top}
   })
 
   xAxisTransformString = computed(() => {
     const yScale = this.dataservice.yScale();
-    return `translate(0, ${yScale.range()[0]})`; // for d3, (0,0) is the upper left hand corner. When looking at data, the lower left hand corner is (0,0)
+    console.log("xAxisTransformString= " + yScale.range()[0]);
+    return `translate(0, ${yScale.range()[0]})`; // for d3, (0,0) is the upper left hand corner. When looking at data, the lower left hand corner is (0,0) 
   });
 
   yAxisTransformString = computed(() => {
     const xScale = this.dataservice.xScale();
+    console.log("yAxisTransformString= " + xScale.range()[0]);
     return `translate(${xScale.range()[0]}, 0)`;
   });
 
@@ -80,5 +83,55 @@ export class GraphComponent {
     const g = this.axesYContainer().nativeElement;
     select(g).transition(transition()).duration(300).call(axisLeft(y));
   });
+
+  updateScrollContainer = effect(() => {
+    const rectSVG = this.svgGraph().nativeElement.getBoundingClientRect();  // Bounding client rectangle of SVG element
+    const scrollBarWidth = 8;
+    const g = this.scrollContainer().nativeElement;
+    const rootG = select(g)
+      .attr("clip-path", "url(#scrollbox-clip-path)");
+    const rectG = g.getBoundingClientRect();  // Bounding client rectangle of g
+    const sizeG = g.getBBox();  // Bounding Box of g
+    const svg = this.svgGraph().nativeElement;
+    const parentSVG = select('svg');
+
+    const contentItems = rootG.selectAll('*')
+    const content = rootG.append('g')
+      .attr('transform', 'translate(' + rectG.x + ',' + rectG.y + ')')
+      .attr('id', 'contentG');
+    contentItems.each( function(p, j) {
+      content.select("#contentG").append(this);
+    })
+  
+    const clipRect = parentSVG.append('clipPath').attr('id', 'scrollbox-clip-path').append('rect');  // Clip everything outside of g rectangle
+    clipRect
+      .attr('x', sizeG.x)
+      .attr('y', sizeG.y)
+      .attr('width', sizeG.width)
+      .attr('height', sizeG.height)
+      .attr('transform', 'translate(' + rectG.x + ',0)');  // x is at 0, so add offset
+
+    rootG  // Add invisible rectangle which catches scroll actions
+      .insert('rect', 'g')
+      .attr('x', sizeG.x)
+      .attr('y', sizeG.y)
+      .attr('width', sizeG.width)
+      .attr('height', sizeG.height)
+      .attr('opacity', 0)
+      .attr('transform', 'translate(' + rectG.x + ',0)');
+
+    /*
+    const testRect = parentSVG.append('rect');
+    testRect
+      .attr('x', sizeG.x)
+      .attr('y', sizeG.y)
+      .attr('width', sizeG.width)
+      .attr('height', sizeG.height)
+      .attr('fill', 'blue')
+      .attr('transform', 'translate(' + rectG.x + ',0)');
+    */
+
+    console.log(sizeG.x + ", " + sizeG.y + ", " + sizeG.width + ", " + sizeG.height);
+  })
 
 }
