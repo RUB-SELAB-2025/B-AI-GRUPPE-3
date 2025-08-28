3db7f331d941a99cfba66695d5798b457e7bd25e
Ataneko161 <info@tentacle.nexus>
2025-08-28 12:35:14 +0200

    Moved tests to graph.component.spec Still not working

--- angular-frontend/src/app/graph/graph.component.spec.ts
+++ angular-frontend/src/app/graph/graph.component.spec.ts
@@ -13,10 +13,18 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GraphComponent } from './graph.component';
+import { StartDataButtonComponent } from '../source-selection/start-data-from-source.component';
+
+import {HarnessLoader} from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import {MatCardHarness} from '@angular/material/card/testing';
+import {MatTestDialogOpenerModule,MatDialogHarness} from '@angular/material/dialog/testing';
 
 describe('GraphComponent', () => {
   let component: GraphComponent;
   let fixture: ComponentFixture<GraphComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -27,10 +35,121 @@ describe('GraphComponent', () => {
 
     fixture = TestBed.createComponent(GraphComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    //fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('finds the Select Data button', async () => {
+      const selectDataButtonExists: boolean = await loader.hasHarness(MatButtonHarness.with({text: 'Select Data'}));
+      expect(selectDataButtonExists).withContext("Button exists with .hasHarness").toBe(true);
+  });
+  it('opens dialog', async () => {
+    const selectDataButton: MatButtonHarness = await loader.getHarness(MatButtonHarness.with({text: 'Select Data'}));
+    await selectDataButton.click();
+    const dialogExists: boolean = await loader.hasHarness(MatDialogHarness);
+    expect(dialogExists).withContext("The Dialog exists").toBe(true);
+  });
+  it('closes dialog', async () => {
+    const selectDataButton: MatButtonHarness = await loader.getHarness(MatButtonHarness.with({text: 'Select Data'}));
+    await selectDataButton.click();
+    const dialog = await loader.getHarness(MatDialogHarness);
+    await dialog.close();
+    const noDialogs = await loader.hasHarness(MatDialogHarness);
+    expect(noDialogs).withContext("Dialog doesn't exist").toBe(false);
+  });
+/*
+  it('shows the modal dialog and the start button works', async () => {
+      const dialogs = await loader.getAllHarnesses(MatDialogHarness);
+      expect(dialogs.length).toBe(1);
+      const dialog = await loader.getHarness(MatDialogHarness);
+      expect(dialog.getTitleText).toBe("Select Data Source");
+
+      const startButtons = await loader.getAllHarnesses(MatButtonHarness.with({text: 'Start'}));
+      expect(startButtons.length).toBe(1);
+      const startButton: MatButtonHarness = await loader.getHarness(MatButtonHarness.with({text: 'Start'}));
+      await startButton.click();
+      expect(await startButton.getText()).toBe("Start");
+      const noDialogs = await loader.hasHarness(MatDialogHarness);
+      expect(noDialogs).toBe(false);
+  });*/
 });
+/*
+describe('StartDataButtonComponent', () => {
+
+    let component: StartDataButtonComponent;
+    let fixture: ComponentFixture<StartDataButtonComponent>;
+    let loader: HarnessLoader;
+    
+
+    beforeEach(async () => {
+        TestBed.configureTestingModule({ imports: [GraphComponent], declarations: [StartDataButtonComponent] });
+        fixture = TestBed.createComponent(StartDataButtonComponent);
+        component = fixture.componentInstance;
+        loader = TestbedHarnessEnvironment.loader(fixture);
+    });
+
+    it('should create', async () => {
+        expect(component).toBeDefined();
+    });
+
+    it('displays buttons correct', async () => {
+        const cards = await loader.getAllHarnesses(MatCardHarness);
+        const buttons = await loader.getAllHarnesses(MatButtonHarness); // length: depending on sources, >= 2
+
+        const checkNumber: number = cards.length + 2;
+        expect(buttons.length).toBe(checkNumber);
+
+        if (cards.length > 0) {
+            for (let card of cards) {
+                let cardButtons = await card.getAllHarnesses(MatButtonHarness);
+                expect(cardButtons.length).toBe(1);
+                expect(await card.hasHarness(MatButtonHarness)).toBe(true);
+                const button: MatButtonHarness = await card.getHarness(MatButtonHarness);
+                expect(await button.getText()).toBe("Select");
+                await button.click();
+                cardButtons = await card.getAllHarnesses(MatButtonHarness);
+                expect(cardButtons.length).toBe(1);
+                expect(await button.getText()).toBe("Delete");
+            }
+        };
+        
+        const clearButtons = await loader.getAllHarnesses(MatButtonHarness.with({text: 'Clear Selection'}));
+        expect(clearButtons.length).toBe(1);
+        const clear: MatButtonHarness = await loader.getHarness(MatButtonHarness.with({text: 'Clear Selection'}));
+        await clear.click();
+        expect(await clear.getText()).toBe("Clear Selection");
+
+        if (cards.length > 0) {
+            for (let card of cards) {
+                let cardButtons = await card.getAllHarnesses(MatButtonHarness);
+                expect(cardButtons.length).toBe(1);
+                expect(await card.hasHarness(MatButtonHarness)).toBe(true);
+                const button: MatButtonHarness = await card.getHarness(MatButtonHarness);
+                expect(await button.getText()).toBe("Select");
+                await button.click();
+                cardButtons = await card.getAllHarnesses(MatButtonHarness);
+                expect(cardButtons.length).toBe(1);
+                expect(await button.getText()).toBe("Delete");
+            }
+        };
+    });
+
+    it('shows the modal dialog and the start button works', async () => {
+        const dialogs = await loader.getAllHarnesses(MatDialogHarness);
+        expect(dialogs.length).toBe(1);
+        const dialog = await loader.getHarness(MatDialogHarness);
+        expect(dialog.getTitleText).toBe("Select Data Source");
+
+        const startButtons = await loader.getAllHarnesses(MatButtonHarness.with({text: 'Start'}));
+        expect(startButtons.length).toBe(1);
+        const startButton: MatButtonHarness = await loader.getHarness(MatButtonHarness.with({text: 'Start'}));
+        await startButton.click();
+        expect(await startButton.getText()).toBe("Start");
+        const noDialogs = await loader.hasHarness(MatDialogHarness);
+        expect(noDialogs).toBe(false);
+    })
+});*/
--- angular-frontend/src/app/source-selection/start-data-from-source.component.spec.ts
+++ angular-frontend/src/app/source-selection/start-data-from-source.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import {HarnessLoader} from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { StartDataButtonComponent } from './start-data-from-source.component';
 
 import {MatButtonHarness} from '@angular/material/button/testing';
 import {MatCardHarness} from '@angular/material/card/testing';
 import {MatTestDialogOpenerModule,MatDialogHarness} from '@angular/material/dialog/testing';
-
+/*
 describe('StartDataButtonComponent', () => {
 
     let component: StartDataButtonComponent;
@@ -81,4 +83,4 @@ describe('StartDataButtonComponent', () => {
         const noDialogs = await loader.hasHarness(MatDialogHarness);
         expect(noDialogs).toBe(false);
     })
-});
+});*/
